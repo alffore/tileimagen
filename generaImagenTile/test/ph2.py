41d68d63e70302bb180bdfb6e1427c40a6ab845c
A. Alfonso F. R <alffore@gmail.com>
2020-10-26 15:05:04 -0600

    Pruebas para comparacion de histogramas

--- generaImagenTile/test/ph2.py
+++ generaImagenTile/test/ph2.py
@@ -7,7 +7,7 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
-img = cv2.imread('~/Downloads/B.jpg')
+img = cv2.imread('/Users/alfonso/Downloads/B.jpg')
 color = ('b', 'g', 'r')
 for i, col in enumerate(color):
     histr = cv2.calcHist([img], [i], None, [256], [0, 256])
